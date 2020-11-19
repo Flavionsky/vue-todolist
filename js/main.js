f89ba12403ca2b25c905a1299b74f77f40acfc12
Flavio Muscente <flaviomuscente@libero.it>
2020-11-19 12:50:37 +0100

    add color click

--- js/main.js
+++ js/main.js
@@ -2,25 +2,40 @@ var app = new Vue({
 el: '#root',
 data: {
   myTodos: [],
-  todo: "",
+    todo: "",
   messageErr: "Inserisci qualcosa da fare!"
 },
 methods:{
-  addTodo: function(){
+  addTodo(){
 
     if(this.todo==""){
 
       alert(this.messageErr);
-      
+
     }else{
-
-      this.myTodos.push(this.todo);
+      let todoObj = {
+        todoel:this.todo,
+        isActive: undefined
+      };
+      this.myTodos.push(todoObj);
       this.todo = "";
-
     }
   },
-  removeTodo: function(index){
+  removeTodo(index){
     this.myTodos.splice(index, 1);
-  }
+  },
+  addColor(index){
+    console.log(this.myTodos.isActive)
+    if (this.myTodos[index].isActive == undefined){
+      this.myTodos[index].isActive = "green"
+    }else{
+      this.myTodos[index].isActive = undefined
+    };
+  },
 }
 });
+
+// rewrite: function(index){
+//   this.myTodos.splice(index, 1);
+// }
+// <a @click="rewrite(index)" href="#"><i class="fas fa-pencil-alt"></i></a>
